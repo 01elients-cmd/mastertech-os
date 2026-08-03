cbd0beddf5e2cbe0ac9a95702de8417dbfcd30ea
01elients-cmd <01elients@gmail.com>
2026-08-03 17:39:05 -0400

    Configure force-dynamic routing for /telegram-panel and add /nube alias route

---
+++ app/nube/page.tsx
@@ -0,0 +1,9 @@
+'use client';
+
+export const dynamic = 'force-dynamic';
+
+import StandaloneTelegramPanel from '../telegram-panel/page';
+
+export default function NubePage() {
+  return <StandaloneTelegramPanel />;
+}
