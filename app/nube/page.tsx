7eb767bc9ebd29f4af60576aa43a09fc9880f292
01elients-cmd <01elients@gmail.com>
2026-08-04 09:33:14 -0400

    Integrate Hilos & Grupos Telegram directly into main navigation sidebar

--- app/nube/page.tsx
+++
@@ -1,9 +0,0 @@
-'use client';
-
-export const dynamic = 'force-dynamic';
-
-import StandaloneTelegramPanel from '../telegram-panel/page';
-
-export default function NubePage() {
-  return <StandaloneTelegramPanel />;
-}
